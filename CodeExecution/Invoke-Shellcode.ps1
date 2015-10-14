00ae6076484e2a2980d9d8cacf3c4d2b6f3eeaf1
PYANTSU <pyantsu@bstudios.moe>
2015-10-14 15:34:04 -0400

    Update Invoke-Shellcode.ps1

--- CodeExecution/Invoke-Shellcode.ps1
+++ CodeExecution/Invoke-Shellcode.ps1
@@ -1,10 +1,3 @@
-# The actual Invoke-Shellcode has moved to Invoke--Shellcode.ps1.
-# This was done to make a point that you have no security sense
-# if you think it's okay to blindly download/exec code directly
-# from a GitHub repo you don't control. This will undoubedtly break
-# many scripts that have this path hardcoded. If you don't like it,
-# fork PowerSploit and host it yourself.
-
 function Invoke-Shellcode
 {
 
@@ -49,6 +42,4 @@ function Invoke-Shellcode
     [Switch]
     $Force = $False
 )
-
-throw 'Something terrible may have just happened and you have no idea what because you just arbitrarily download crap from the Internet and execute it.'
 }
